1bb27add68934009c092d1387e69eca17b7680c5
Johan Driessen <johan@driessen.se>
2017-05-08 11:50:39 +0200

    Replace xmlns:xlink and xlink:href with their React-compatible attributes

--- src/helpers/cleanup.js
+++ src/helpers/cleanup.js
@@ -10,6 +10,8 @@ module.exports.cleanupSvg = (svg) => {
     .replace(/clip-rule="#?\w+"/g, '')
     .replace(/\s{2,}/g, ' ')
     .replace(/class="\w+"/g, match => match.replace(/class/g, 'className'))
+    .replace(/xmlns:xlink="[\S]+"/g, match => match.replace(/xmlns:xlink/g, 'xmlnsXlink'))
+    .replace(/xlink:href="[\S]+"/g, match => match.replace(/xlink:href/g, 'xlinkHref'))
     .replace(/<style>.*<\/style>/g, match => match.replace(/<style>/g, '<style>{ `').replace(/<\/style>/g, '` }</style>'))
     .replace(/viewBox/, 'fill={color} height={height || size} width={width || size} onClick={onClick} style={style} className={className} viewBox');
 };
